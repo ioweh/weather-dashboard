44e336638eb9aa3afb7c2fcbc2875a0c7aea90d2
Oleg Ilinets <oleg.ilinets@gmail.com>
2023-07-28 00:07:17 +0200

    Changed api to https

--- public/js/components/CityList/index.tsx
+++ public/js/components/CityList/index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const CityList = (): JSX.Element => {
     const [citiesWithForecast, setCitiesWithForecast] = useState<any>([]);
     const [cityName, setCityName] = useState("London");
-    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=b35ef7587dfa519e18d36e86584481a2`;
+    let url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=b35ef7587dfa519e18d36e86584481a2`;
     
     useEffect(() => {
         const fetchData = async () => {
