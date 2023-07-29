b3d25704c6d49e980d95d06152c9c5f2c635e353
Oleg Ilinets <oleg.ilinets@gmail.com>
2023-07-29 19:27:05 +0200

    Added geolocation and redirect on page load

--- public/js/components/CityList/index.tsx
+++ public/js/components/CityList/index.tsx
@@ -1,12 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const CityList = (): JSX.Element => {
     const [citiesWithForecast, setCitiesWithForecast] = useState<any>([]);
     const [cityName, setCityName] = useState("London");
+    const [latitude, setLatitude] = useState<any>(null);
+    const [longitude, setLongitude] = useState<any>(null);
+    const navigate = useNavigate();
 
     let url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=b35ef7587dfa519e18d36e86584481a2`;
+    
+    const getGeolocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    // Handle successful geolocation retrieval
+                    setLatitude(position.coords.latitude);
+                    setLongitude(position.coords.longitude);
+                },
+                (error) => {
+                    // Handle geolocation error
+                    console.error('Error getting geolocation:', error.message);
+                }
+            );
+        } else {
+            console.error('Geolocation is not supported by this browser.');
+        }
+    };
     
     useEffect(() => {
         const fetchData = async () => {
@@ -18,9 +39,15 @@ const CityList = (): JSX.Element => {
                 console.error('Error fetching data:', error);
             }
         };
+
+        getGeolocation();
+        const goTo5DayForecastPage = () => navigate(`/forecast/${latitude}/${longitude}`);
+        if (latitude && longitude) {
+            goTo5DayForecastPage();
+        }
         
         fetchData(); // Call the async function to fetch data when the component mounts
-    }, [cityName]);
+    }, [cityName, latitude, longitude]);
 
     return (
     <>
