4b99bcb136ecc3da278e18919306775a31c7c465
Oleg Ilinets <oleg.ilinets@gmail.com>
2023-08-01 22:25:11 +0200

    Added tabs for humidity and wind in details

--- public/js/components/CityDetailedForecast/index.tsx
+++ public/js/components/CityDetailedForecast/index.tsx
@@ -17,17 +17,17 @@ ChartJS.register(
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
 );
 
 import './index.less';
+import TabsComponent from '../TabsComponent';
 
 const CityDetailedForecast = (): JSX.Element => {
     const location = useLocation();
 
     // get detailed forecast
     const data = location.state;
-    console.log(data);
 
     function kelvinToCelsius(kelvin) {
         // Check if the input is a valid number
@@ -41,7 +41,7 @@ const CityDetailedForecast = (): JSX.Element => {
         return celsius.toFixed(1);
     }
 
-    const options = {
+    const temperatureOptions = {
         responsive: true,
         plugins: {
             legend: {
@@ -49,26 +49,89 @@ const CityDetailedForecast = (): JSX.Element => {
             },
             title: {
                 display: true,
-                text: 'Temperature Chart',
+                text: 'Temperature, °C',
+            },
+        },
+    };
+
+    const humidityOptions = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: true,
+                text: 'Humidity, %',
+            },
+        },
+    };
+
+    const windOptions = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: true,
+                text: 'Wind, m/s',
             },
         },
     };
 
     const labels = data.list.map(forecast => forecast.dt_txt);
     
-    const chartData = {
+    const temperatureData = {
         labels,
         datasets: [
             {
-                label: data.city.name,
+                label: data.city?.name,
                 data: data.list.map(forecast => kelvinToCelsius(forecast.main.temp)),
                 backgroundColor: 'rgba(255, 99, 132, 0.5)',
             },
         ],
     };
 
+    const humidityData = {
+        labels,
+        datasets: [
+            {
+                label: data.city?.name,
+                data: data.list.map(forecast => forecast.main.humidity),
+                backgroundColor: 'rgba(140, 155, 181, 0.5)',
+            }
+        ]
+    }
+
+    const windData = {
+        labels,
+        datasets: [
+            {
+                label: data.city?.name,
+                data: data.list.map(forecast => forecast.wind.speed),
+                backgroundColor: 'rgba(171, 185, 162, 0.5)',
+            }
+        ]
+    }
+
+    const tabsData = [
+    {
+      label: 'Temperature',
+      content: <Bar options={temperatureOptions} data={temperatureData} />,
+    },
+    {
+      label: 'Humidity',
+      content: <Bar options={humidityOptions} data={humidityData} />,
+    },
+    {
+      label: 'Wind',
+      content: <Bar options={windOptions} data={windData} />,
+    },
+  ];
+
     return (
-        <Bar options={options} data={chartData} />
+        <TabsComponent tabsData={tabsData} />
     );
 }
 
---
+++ public/js/components/TabsComponent/index.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './index.less';
+
+const TabsComponent = ({ tabsData }) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <div className="tabs-container">
+      <div className="tabs-container__tabs">
+        {tabsData.map((tab, index) => (
+          <div
+            key={index}
+            className={`tabs-container__tabs__tab ${activeTab === index ? 'tabs-container__tabs__tab--active' : ''}`}
+            onClick={() => handleTabClick(index)}
+          >
+            {tab.label}
+          </div>
+        ))}
+      </div>
+      <div className="tabs-container__tab-content">{tabsData[activeTab].content}</div>
+    </div>
+  );
+};
+
+export default TabsComponent;
