dcb66d7adc7b651983e8901c381e8854f420068a
Oleg Ilinets <oleg.ilinets@gmail.com>
2023-08-02 23:52:02 +0200

    Added more tests for tabs component

--- public/js/components/TabsComponent/index.test.tsx
+++ public/js/components/TabsComponent/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { act, render } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react';
 import TabsComponent from './index';
 import '@testing-library/jest-dom/extend-expect';
 
@@ -37,5 +37,29 @@ describe('TabsComponent', () => {
     const tabs = container.getElementsByClassName('tabs-container__tabs__tab');
 
     expect(tabs).toHaveLength(3);
+  });
+
+  it('should be able to move to the humidity tab', async () => {
+    // Mount the component
+    let container;
+    await act( async () => ({container} = render(<TabsComponent tabsData={tabsData} />)));
+    const tabs = container.getElementsByClassName('tabs-container__tabs__tab');
+    const secondTab = tabs[1];
+    fireEvent.click(secondTab);
+    const currentTab = container.getElementsByClassName('tabs-container__tab-content')[0];
+
+    expect(currentTab).toHaveTextContent("Humidity content");
+  });
+
+  it('should be able to move to the wind tab', async () => {
+    // Mount the component
+    let container;
+    await act( async () => ({container} = render(<TabsComponent tabsData={tabsData} />)));
+    const tabs = container.getElementsByClassName('tabs-container__tabs__tab');
+    const secondTab = tabs[2];
+    fireEvent.click(secondTab);
+    const currentTab = container.getElementsByClassName('tabs-container__tab-content')[0];
+
+    expect(currentTab).toHaveTextContent("Wind content");
   });
 });
